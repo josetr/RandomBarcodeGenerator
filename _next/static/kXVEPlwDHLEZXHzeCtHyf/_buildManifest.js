12f311aae1be089d0d9a17c42538df5b39c3028b
josetr <josetr@users.noreply.github.com>
2022-05-07 04:50:06 +0000

    deploy: 1e8817b5725f88fe34044b9afccee07fda8da907

---
+++ _next/static/kXVEPlwDHLEZXHzeCtHyf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/68-2ed1393122001a77.js","static/css/30f46f89f8f9a13f.css","static/chunks/pages/index-16763083002757a5.js"],"/_error":["static/chunks/pages/_error-49f0f5318dc60976.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
